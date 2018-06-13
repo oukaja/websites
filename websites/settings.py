eb163b59e4f389e36256740fbe530ea650c0c1bf
OUKAJA <oukaja.mehdi@gmail.com>
2018-06-13 02:16:50 +0000

    v

--- websites/settings.py
+++ websites/settings.py
@@ -72,9 +72,9 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    'websites.pipelines.AppPipeline': 300,
-# }
+ITEM_PIPELINES = {
+   'websites.pipelines.AppPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
