1470b5d6c1b13f2aaff0522840748d86f7612e0e
OUKAJA <oukaja.mehdi@gmail.com>
2018-06-30 17:09:49 +0100

    v

---
+++ websites/cmds.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+scrapy crawl alanba
+scrapy crawl alarabiya
+scrapy crawl almasryalyoum
+scrapy crawl arabicpost
+scrapy crawl baljazeera
+scrapy crawl bbc
+scrapy crawl blogsaljazeera
+scrapy crawl blogsaljazeera2
+scrapy crawl hespressf
+scrapy crawl hibapress
+scrapy crawl kapitalis
+scrapy crawl kol7sry
+scrapy crawl midanaljazeera
+scrapy crawl qatarliving
+scrapy crawl theguardian
+scrapy crawl thmnia
+scrapy crawl tunisia
+scrapy crawl worldbank
+scrapy crawl youm7
+
